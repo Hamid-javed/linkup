817e1f955619e794691cdde1d0027c2cb2a4eceb
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-08-30 05:55:51 -0700

    "inital commit"

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const AboutUs = () => {
+  return (
+    <>
+    <div className='text-2xl bg-black'>AboutUs</div>
+    <div className='text-2xl bg-black font-thin'>AboutUs</div>
+    
+    </>
+  )
+}
+
+export default AboutUs
