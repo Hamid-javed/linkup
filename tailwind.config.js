94ef5092786b685edf00e813c927238c13a6db64
Hamid <hamidjaved615@gmail.com>
2024-08-31 15:58:33 +0500

    Fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Make sure to include the correct file extensions for your framework
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
