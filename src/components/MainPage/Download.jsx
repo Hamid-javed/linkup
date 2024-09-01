4abd7f67ee7242d76b63f62466d2e2ed91f23023
Hamid <hamidjaved615@gmail.com>
2024-09-01 19:05:36 +0500

    added download component

--- src/components/MainPage/Download.jsx
+++ src/components/MainPage/Download.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Download = () => {
   return (
-    <div className='bg-[#1E698E]'>
-
+    <div className='bg-[#1E698E] px-10 py-24'>
+        <div className="bg-white w-full flex justify-between items-center py-20 px-20 rounded-3xl">
+            <div className="">
+                <h1 className='text-4xl font-bold text-[#1E698E]'>Stay connected, stay social  with LinkUp!</h1>
+                <p className='text-[#1E698E]'>Become a part of our growing community and start your journey today.</p>
+            </div>
+            <button className='border-2 border-[#1E698E] rounded-3xl px-7 py-3 active:bg-[#1E698E] active:text-white'>Download</button>
+        </div>
     </div>
   )
 }
