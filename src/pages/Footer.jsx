79b877a3b3fd7d6bdcd41390c9f3ff39a91064d5
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-09-01 09:00:36 -0700

    FINAL COMMIT

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,10 +4,10 @@ import { LuActivitySquare } from "react-icons/lu";
 
 const Footer = () => {
   return (
-    <div className="bg-[#1e6a8e] text-white p-2 mx-4 mt-5">
+    <div className="bg-[#1e6a8e] text-white p-2 lg:mx-10 mx-6 mt-5">
       <div className="block lg:flex lg:justify-between lg:items-start lg:space-x-10 text-center lg:text-left">
         <div className="lg:w-1/3 mb-6 lg:mb-0 mx-auto lg:mx-0 pb-0">
-          <p className="text-sm lg:text-base ">
+          <p className="text-medium lg:text-base ">
             We believe in the power of social media to bring people together and
             facilitate global communication. LinkUp empowers users to express
             themselves, share information, and interact with a broad audience in
@@ -16,11 +16,17 @@ const Footer = () => {
         </div>
         <div className="flex flex-col lg:flex-row lg:space-x-10 text-center lg:text-left">
           <div className="mb-6 lg:mb-0 mx-auto lg:mx-0">
-            <b className="block mb-3 lg:mb-2">Pages</b>
+            <b className="block mb-3 lg:mb-2 ">Pages</b>
             <ul className="list-none space-y-2 text-sm lg:text-base">
-              <li>About Us</li>
-              <li>Contact Us</li>
-              <li>Privacy Policy</li>
+              <li>
+                <a className="hover:text-blue-500"  href="/About"> About  Us</a>
+              </li>
+              <li>
+                <a className="hover:text-blue-500" href="/About"> Contact Us</a>{" "}
+              </li>
+              <li>
+                <a className="hover:text-blue-500" href="/About"> Privacy Policy</a>
+              </li>
             </ul>
           </div>
           <div className="mx-auto lg:mx-0">
@@ -28,10 +34,20 @@ const Footer = () => {
             <p className="text-sm lg:text-base mb-3">
               Follow our social media for the latest updates and news.
             </p>
-            <div className="flex justify-center lg:justify-start gap-4 text-white text-xl">
-              <LuActivitySquare />
-              <FaInstagram />
-              <FaFacebookF />
+            <div className="flex justify-center lg:justify-start gap-2 text-white text-xl">
+              <a className="hover:text-blue-500" href="">
+                {" "}
+                <LuActivitySquare />{" "}
+              </a>
+
+              <a className="hover:text-red-600" href="">
+                {" "}
+                <FaInstagram />{" "}
+              </a>
+              <a className="hover:text-blue-700" href="">
+                {" "}
+                <FaFacebookF />{" "}
+              </a>
             </div>
           </div>
         </div>
@@ -39,7 +55,7 @@ const Footer = () => {
       <div className="mt-6 border-t border-white"></div>
       <div className="text-center mt-4">
         <small className="text-xs lg:text-sm">
-          Copyright © 2024. All rights reserved.
+        HELLO WORLD TECHNOLOGIES   All rights reserved Copyright © 2024.
         </small>
       </div>
     </div>
